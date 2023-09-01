1108ad53089aa78fe1f15458d394c51652057c1c
billy <Billy@mask.io>
2023-09-01 15:42:32 +0800

    chore: cors setting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true };
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        },
+      ],
+    },
+  ],
+};
 
 module.exports = nextConfig;
