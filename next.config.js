4b82e9d3e4ec65faa2d3953bf492daf857d2d2b4
billy <Billy@mask.io>
2025-04-30 16:59:33 +0800

    chore: fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -24,9 +23,5 @@ const nextConfig = {
     ];
   },
 };
-
-if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
-}
 
 module.exports = nextConfig;
